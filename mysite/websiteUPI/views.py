b6d3e03846dca1347d381c56474206e251b388cb
unknown <Sachit.Gupta@skylinesemi.com>
2022-10-17 22:16:24 +0530

    created send_email func

--- mysite/websiteUPI/views.py
+++ mysite/websiteUPI/views.py
@@ -30,26 +30,67 @@ def getFile(request):
     print(getLatestObject.file)
     return render(request, 'upi.html' , {})
 
-def sendOutput():
-    try:    
-        print("starting-----------------")
-        super_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        print(dir_path)
-        file_path = super_dir_path + '\\Input inputSuperBOMs in this excel.xlsm'
-        print(file_path)
-        path= r"C:\Users\guptash\OneDrive - Lam Research\Documents\bitbucket\djangoSampleApp\mysite\websiteUPI\businessLogic\Outputs\Unique Parts Output 2020-07-10 09.59 PM.xlsx"
+# def sendOutput():
+#     try:    
+#         print("starting-----------------")
+#         super_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+#         dir_path = os.path.dirname(os.path.realpath(__file__))
+#         print(dir_path)
+#         file_path = super_dir_path + '\\Input inputSuperBOMs in this excel.xlsm'
+#         print(file_path)
+#         path= r"C:\Users\guptash\OneDrive - Lam Research\Documents\bitbucket\djangoSampleApp\mysite\websiteUPI\businessLogic\Outputs\Unique Parts Output 2020-07-10 09.59 PM.xlsx"
+#         path=path.replace("\\","/")
+#         print(path)
+#         c=datetime.now()
+#         msg = EmailMessage('Testing-----------'+str(datetime.now()), 'Body of the email', 'shikhar.Gupta@lamresearch.com', ['shikhar.kumar@orbitsolutions.net'])
+#         msg.content_subtype = "html"
+#         file=open(path,'rb')
+#         msg.attach('test.xlsx',file.read(),'application/ms-excel')
+#         msg.send()
+#         print("mail sent------")
+#     except SMTPException as e:
+#         print("-------------0----------")
+#     return '-----Email Sent----'
+
+
+def send_mail():
+    path="D:\office\djangoSampleSystemApp\Forecast.csv"
+    path=path.replace("\\","/")
+    print("-------outlook -------")
+    import win32com.client
+    outlook = win32com.client.Dispatch('outlook.application')
+    mail = outlook.CreateItem(0)
+    mail.To = 'Sachit.Gupta@orbitsolutions.net'
+    mail.Subject = 'Sample Email'
+    mail.HTMLBody = '<h3>This is HTML Body</h3>'
+    mail.Body = "This is the normal body"
+    mail.Attachments.Add(path)
+    # mail.CC = 'somebody@company.com'
+    mail.Send()
+    print("-----mail sent--------")
+
+def email_send(path):
+    try:
+        print("-----------------")
+        path="D:\office\djangoSampleSystemApp\Forecast.csv"
         path=path.replace("\\","/")
-        print(path)
         c=datetime.now()
-        msg = EmailMessage('Testing-----------'+str(datetime.now()), 'Body of the email', 'shikhar.Gupta@lamresearch.com', ['shikhar.kumar@orbitsolutions.net'])
+        msg = EmailMessage('Testing-----------'+str(datetime.now()), 'Body of the email', 'Sachit.Gupta@skylinesemi.com', ['Sachit.Gupta@orbitsolutions.net'])
         msg.content_subtype = "html"
         file=open(path,'rb')
         msg.attach('test.xlsx',file.read(),'application/ms-excel')
         msg.send()
         print("mail sent------")
+        return '-----Email Sent----'
     except SMTPException as e:
         print("-------------0----------")
-    return '-----Email Sent----'
+        return
 
-sendOutput()
+def download_csv(request):    
+    # file_path = os.path.join(settings.MEDIA_ROOT, 'example-input-file.txt')   
+    file_path="D:\office\djangoSampleSystemApp\Forecast.csv" 
+    if os.path.exists(file_path):    
+        with open(file_path, 'rb') as fh:    
+            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")    
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)    
+            return r
